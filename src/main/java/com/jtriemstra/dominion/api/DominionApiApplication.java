e27288243c8dbb67e6357dde7a8805e7b0d3e7a6
Joel Triemstra <>
2019-12-30 09:18:27 -0500

    initial files from start.spring.io

---
+++ src/main/java/com/jtriemstra/dominion/api/DominionApiApplication.java
@@ -0,0 +1,13 @@
+package com.jtriemstra.dominion.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DominionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DominionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jtriemstra/dominion/api/DominionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtriemstra.dominion.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DominionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
